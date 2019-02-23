b06e2dddd2f099ce9526e35e7c922098448c4ce7
Alexandr Khomichevskiy <khomichevskiy@gmail.com>
2019-02-23 19:29:51 +0300

    1.3 JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ *Class Calculate решение задачи части 001.
+ *@author Alexandr Khomichevskiy
+ *@version 1.0
+ *@since 1.0
+ */
 public class Calculate {
+	/**
+	 *Метод main.
+	 *@param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *package-info file for calcalate package.
+ *@author Alexandr Khomichevskiy
+ *@version 1.0
+ *@since 1.0
+ */
+
+package ru.job4j.calculate;
