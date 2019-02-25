1ed5b7b2d520cce824228cfb12a1ff2ff9d9232e
Alexandr Khomichevskiy <khomichevskiy@gmail.com>
2019-02-25 22:10:18 +0300

    3.4. Добавление тестов к классу Point.

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexandr Khomichevskiy.
+ * @version 1.0.
+ * @since 25.02.19.
+ */
+public class PointTest {
+    @Test
+    public void pointTestOne() {
+        Point pointA = new Point(1, 4);
+        Point pointB = new Point(2, 2);
+        double expect = 2.23606797749979;
+        assertThat(pointA.distanceTo(pointB), closeTo(expect, 0));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Condition package.
+ *
+ * @author Alexandr Khomichevskiy.
+ * @version 1.0.
+ * @since 25.02.19.
+ */
+package ru.job4j.condition;
