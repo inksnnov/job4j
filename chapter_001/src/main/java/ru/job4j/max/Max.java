fcf70c74ed469e41a8e1dabc6e15d0822f1c3388
Alexandr Khomichevskiy <khomichevskiy@gmail.com>
2019-02-27 22:42:02 +0300

    4.4. Максимум из трех чисел. Add Javadoc to max method.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -20,6 +20,14 @@ public class Max {
         return first > second ? first : second;
     }
 
+    /**
+     * Метод возвращает максимальное число из трех принятых агрументами.
+     *
+     * @param first  int первое число.
+     * @param second int второе число.
+     * @param third  int третье число.
+     * @return int третье число.
+     */
     public int max(int first, int second, int third) {
         return this.max(this.max(first, second), third);
     }
