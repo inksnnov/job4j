09a0e7223f5ed6d4200289714466297053d2e8a9
Alexandr Khomichevskiy <khomichevskiy@gmail.com>
2019-03-23 18:15:39 +0300

    Рефакторинг теста PaintTest с помощью @Before @After.

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -14,16 +16,41 @@ import static org.junit.Assert.*;
  */
 public class PaintTest {
 
+    private final PrintStream stdout = System.out;
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("Выполнение перед началом теста.");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("Выполнение после теста.");
+    }
+
     @Test
-    public void toConsoleOutTest() {
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    public void toConsoleOutTestOne() {
         new Paint(new Square()).toConsoleOut();
         assertThat(out.toString(), is(new StringBuilder()
                 .append("+++++++")
                 .append("+     +")
                 .append("+     +")
+                .append("+++++++")
+                .append(System.lineSeparator())
+                .toString()));
+    }
+
+    @Test
+    public void toConsoleOutTestTwo() {
+        new Paint(new Triangle()).toConsoleOut();
+        assertThat(out.toString(), is(new StringBuilder()
+                .append("   +   ")
+                .append("  +++  ")
+                .append(" +++++ ")
                 .append("+++++++")
                 .append(System.lineSeparator())
                 .toString()));
