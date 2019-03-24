1cf4e345a0dacb870fdc47b6111ea2d6f7025360
Alexandr Khomichevskiy <khomichevskiy@gmail.com>
2019-03-24 18:29:52 +0300

    Добавление тестов для класса StartUI.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -12,12 +17,40 @@ import static org.junit.Assert.*;
  */
 public class StartUITest {
 
+    private final PrintStream stdout = System.out;
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
     @Test
     public void createItemTestOne() {
         Tracker tracker = new Tracker();
         Input input = new StubInput(new String[]{"0", "testName", "desc", "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("testName"));
+    }
+
+    private String showMenu() {
+        String ln = System.lineSeparator();
+        return new StringBuilder()
+                .append("Меню").append(ln)
+                .append("0 - Добавление заявки.").append(ln)
+                .append("1 - Замена заявки.").append(ln)
+                .append("2 - Удаление заявки.").append(ln)
+                .append("3 - Показать все заявки.").append(ln)
+                .append("4 - Поиск заявки по имени.").append(ln)
+                .append("5 - Поиск заявки по Id.").append(ln)
+                .append("6 - Выход из программы.").append(ln)
+                .toString();
     }
 
     @Test
@@ -46,5 +79,84 @@ public class StartUITest {
         assertNull(tracker.findById(itemTwo.getId()));
     }
 
+    @Test
+    public void showAllItemTestOne() {
+        Tracker tracker = new Tracker();
+        long create = System.currentTimeMillis();
+        Item itemOne = new Item("NameOne", "DescOne", create);
+        Item itemTwo = new Item("NameTwo", "DescTwo", create);
+        tracker.add(itemOne);
+        tracker.add(itemTwo);
+        String ln = System.lineSeparator();
+        Input input = new StubInput(new String[]{"3", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(out.toString(), is(new StringBuilder()
+                .append(this.showMenu())
+                .append("------------Все заявки------------").append(ln)
+                .append("Имя заявки :  NameOne ").append(ln)
+                .append("Описание заявки : ").append(ln)
+                .append(" DescOne ")
+                .append(ln)
+                .append(" ")
+                .append(ln)
+                .append("Имя заявки :  NameTwo ").append(ln)
+                .append("Описание заявки : ").append(ln)
+                .append(" DescTwo ")
+                .append(ln)
+                .append(" ")
+                .append(ln)
+                .append(this.showMenu())
+                .toString()
+        ));
+    }
 
+    @Test
+    public void findByNameItemsTestOne() {
+        Tracker tracker = new Tracker();
+        long create = System.currentTimeMillis();
+        Item itemOne = new Item("NameOne", "DescOne", create);
+        Item itemTwo = new Item("NameTwo", "DescTwo", create);
+        tracker.add(itemOne);
+        tracker.add(itemTwo);
+        String ln = System.lineSeparator();
+        Input input = new StubInput(new String[]{"4", "NameOne", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(out.toString(), is(new StringBuilder()
+                .append(this.showMenu())
+                .append("Поиск заявок по имени.").append(ln)
+                .append("Имя заявки :  NameOne ").append(ln)
+                .append("Описание заявки : ").append(ln)
+                .append(" DescOne ")
+                .append(ln)
+                .append(" ")
+                .append(ln)
+                .append(this.showMenu())
+                .toString()
+        ));
+    }
+
+    @Test
+    public void findByIdItemsTestOne() {
+        Tracker tracker = new Tracker();
+        long create = System.currentTimeMillis();
+        Item itemOne = new Item("NameOne", "DescOne", create);
+        Item itemTwo = new Item("NameTwo", "DescTwo", create);
+        tracker.add(itemOne);
+        tracker.add(itemTwo);
+        String ln = System.lineSeparator();
+        Input input = new StubInput(new String[]{"5", itemTwo.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(out.toString(), is(new StringBuilder()
+                .append(this.showMenu())
+                .append("Поиск заявки по ID").append(ln)
+                .append("Имя заявки :  NameTwo ").append(ln)
+                .append("Описание заявки : ").append(ln)
+                .append(" DescTwo ")
+                .append(ln)
+                .append(" ")
+                .append(ln)
+                .append(this.showMenu())
+                .toString()
+        ));
+    }
 }
