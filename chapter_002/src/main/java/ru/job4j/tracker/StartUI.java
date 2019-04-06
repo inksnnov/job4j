7045143bad8c64fbb5fc9d40ac675284ec5a2d67
Alexandr Khomichevskiy <khomichevskiy@gmail.com>
2019-04-06 18:19:56 +0300

    Рефакторинг - Шаблон Декоратор для валидатора.

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -59,6 +59,6 @@ public class StartUI {
      * Точка входа в программу.
      */
     public static void main(String[] args) {
-        new StartUI(new ValidateInput(), new Tracker()).init();
+        new StartUI(new ValidateInput(new ConsoleInput()), new Tracker()).init();
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,16 +1,41 @@
 package ru.job4j.tracker;
 
 /**
- * Класс пользовательского ввода с обработкой исключений.
+ * Декоратор пользовательского ввода с обработкой исключений.
  *
  * @author Alexandr Khomichevskiy.
  * @version 1.0.
  */
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
 
     /**
-     * Переопределенный метод пользовательского ввода с обработкой исключений
-     * при неверном вводе.
+     * Объект типа Input.
+     */
+    private Input input;
+
+    /**
+     * Конструктор принимает объект Input и записывает в поле input;
+     *
+     * @param input Input.
+     */
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
+
+    /**
+     * Переопределенный и перегруженный метод ask.Для консольного ввода.
+     *
+     * @param question задаем вопрос пользователю.
+     * @return пользовательский ввод.
+     */
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
+    /**
+     * Переопределенный и перегруженный метод пользовательского ввода с обработкой исключений
+     * при неверном вводе.Служит для корректного выбора пункта меню.
      *
      * @param question String Вопрос пользователю.
      * @param range    int[]  массив элементов меню.
@@ -22,7 +47,7 @@ public class ValidateInput extends ConsoleInput {
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Введите пункт меню из диапазона значений.");
